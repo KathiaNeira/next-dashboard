a633a5cb698c98f6dfc3bee3cba4dde6af25d679
Kathia Neira <kathianeira@gmail.com>
2026-08-12 16:00:54 -0500

    feat: Adding customers view

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-	return <p>Customers page!</p>
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
